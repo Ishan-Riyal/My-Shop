ac0d4c39706b3d0dfe6772ef9d4f64f1504f8736
Ishan Riyal <ishanriyal07@gmail.com>
2026-06-04 01:35:10 +0530

    Fix CORS and Vercel proxy

--- Backend/server.js
+++ Backend/server.js
@@ -19,9 +19,20 @@ connectDB();
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://my-shop-frontend-seven.vercel.app",
+      process.env.CLIENT_URL,
+    ],
     credentials: true,
   }),
 );
